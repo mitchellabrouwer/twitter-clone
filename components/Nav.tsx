aa2754ede45be57c2a1671e06a59f57504495813
mitchellabrouwer <mbarron198@gmail.com>
2022-06-08 10:16:44 +1000

    feat: nav

---
+++ components/Nav.tsx
@@ -0,0 +1,11 @@
+import { BsTwitter } from "react-icons/bs";
+import ToggleDark from "./ToggleDark";
+
+export default function Nav() {
+  return (
+    <>
+      <BsTwitter size="40" />
+      <ToggleDark />
+    </>
+  );
+}
